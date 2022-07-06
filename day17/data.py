853188b88e791f2f0827de834a42e60a96805900
cookieflakes2 <101045565+cookieflakes2@users.noreply.github.com>
2022-07-06 14:14:11 +0300

    Day 16: Quiz Project update

--- day17/data.py
+++ day17/data.py
@@ -7,27 +7,78 @@ question_data = [
         "correct_answer": "True",
         "incorrect_answers": ["False"]
     },
-    {"category": "Science: Computers", "type": "boolean", "difficulty": "medium",
-     "question": "The common software-programming acronym &quot;I18N&quot; comes from the term &quot;Interlocalization&quot;.",
-     "correct_answer": "False", "incorrect_answers": ["True"]},
-    {"category": "Science: Computers", "type": "boolean", "difficulty": "medium",
-     "question": "The first computer bug was formed by faulty wires.", "correct_answer": "False",
-     "incorrect_answers": ["True"]}, {"category": "Science: Computers", "type": "boolean", "difficulty": "medium",
-                                      "question": "FLAC stands for &quot;Free Lossless Audio Condenser&quot;&#039;",
-                                      "correct_answer": "False", "incorrect_answers": ["True"]},
-    {"category": "Science: Computers", "type": "boolean", "difficulty": "medium",
-     "question": "&quot;Windows NT&quot; is a monolithic kernel.", "correct_answer": "False",
-     "incorrect_answers": ["True"]}, {"category": "Science: Computers", "type": "boolean", "difficulty": "medium",
-                                      "question": "The open source program Redis is a relational database server.",
-                                      "correct_answer": "False", "incorrect_answers": ["True"]},
-    {"category": "Science: Computers", "type": "boolean", "difficulty": "medium",
-     "question": "Early RAM was directly seated onto the motherboard and could not be easily removed.",
-     "correct_answer": "True", "incorrect_answers": ["False"]},
-    {"category": "Science: Computers", "type": "boolean", "difficulty": "medium",
-     "question": "The first dual-core CPU was the Intel Pentium D.", "correct_answer": "False",
-     "incorrect_answers": ["True"]}, {"category": "Science: Computers", "type": "boolean", "difficulty": "medium",
-                                      "question": "Linus Sebastian is the creator of the Linux kernel, which went on to be used in Linux, Android, and Chrome OS.",
-                                      "correct_answer": "False", "incorrect_answers": ["True"]},
-    {"category": "Science: Computers", "type": "boolean", "difficulty": "medium",
-     "question": "A Boolean value of &quot;0&quot; represents which of these words?", "correct_answer": "False",
-     "incorrect_answers": ["True"]}]
+    {
+        "category": "Science: Computers",
+        "type": "boolean",
+        "difficulty": "medium",
+        "question": "The common software-programming acronym 'I18N' comes from "
+                    "the term 'Interlocalization'.",
+        "correct_answer": "False",
+        "incorrect_answers": ["True"]
+    },
+    {
+        "category": "Science: Computers",
+        "type": "boolean",
+        "difficulty": "medium",
+        "question": "The first computer bug was formed by faulty wires.",
+        "correct_answer": "False",
+        "incorrect_answers": ["True"]
+    },
+    {
+        "category": "Science: Computers",
+        "type": "boolean",
+        "difficulty": "medium",
+        "question": "FLAC stands for 'Free Lossless Audio Condenser'`",
+        "correct_answer": "False",
+        "incorrect_answers": ["True"]
+    },
+    {
+        "category": "Science: Computers",
+        "type": "boolean",
+        "difficulty": "medium",
+        "question": "'Windows NT' is a monolithic kernel.",
+        "correct_answer": "False",
+        "incorrect_answers": ["True"]
+    },
+    {
+        "category": "Science: Computers",
+        "type": "boolean",
+        "difficulty": "medium",
+        "question": "The open source program Redis is a relational database server.",
+        "correct_answer": "False",
+        "incorrect_answers": ["True"]
+    },
+    {
+        "category": "Science: Computers",
+        "type": "boolean",
+        "difficulty": "medium",
+        "question": "Early RAM was directly seated onto the motherboard and could not be easily removed.",
+        "correct_answer": "True",
+        "incorrect_answers": ["False"]
+    },
+    {
+        "category": "Science: Computers",
+        "type": "boolean",
+        "difficulty": "medium",
+        "question": "The first dual-core CPU was the Intel Pentium D.",
+        "correct_answer": "False",
+        "incorrect_answers": ["True"]
+    },
+    {
+        "category": "Science: Computers",
+        "type": "boolean",
+        "difficulty": "medium",
+        "question": "Linus Sebastian is the creator of the Linux kernel, which "
+                    "went on to be used in Linux, Android, and Chrome OS.",
+        "correct_answer": "False",
+        "incorrect_answers": ["True"]
+    },
+    {
+        "category": "Science: Computers",
+        "type": "boolean",
+        "difficulty": "medium",
+        "question": "A Boolean value of &quot;0&quot; represents which of these words?",
+        "correct_answer": "False",
+        "incorrect_answers": ["True"]
+    }
+]
