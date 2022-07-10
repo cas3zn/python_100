51cbfcb72f2bb71c41d38423b7fb46a9b955ab15
cookieflakes2 <101045565+cookieflakes2@users.noreply.github.com>
2022-07-10 20:11:27 +0300

    Day 24: Files, Directories and Paths

--- day24/main.py
+++ day24/main.py
@@ -1,8 +1,21 @@
-# with open("my_file.txt") as file:
-#     contents = file.read()
-#     print(contents)
+# * Create a letter using starting_letter.txt
+# for each name in invited_names.txt
+# Replace the [name] placeholder with the actual name.
+# Save the letters in the folder "ReadyToSend".
 
-# with open("new_file.txt", mode="w") as file:
-#     file.write("I created a new file.")
+# Hint1: This method will help you: https://www.w3schools.com/python/ref_file_readlines.asp
+# Hint2: This method will also help you: https://www.w3schools.com/python/ref_string_replace.asp
+# Hint3: THis method will help you: https://www.w3schools.com/python/ref_string_strip.asp
 
-pass
+with open("input/letters/starting_letter.txt") as starting_letter:
+    starting_letter_contents = starting_letter.read()
+    invited_names = open("input/names/invited_names.txt")
+    invite = invited_names.readlines()
+    print(invite)
+    for line in invite:
+        line_strip = line.strip()
+        replaced_name = starting_letter_contents.replace("[name]", line_strip)
+        with open(f"./Output/ReadyToSend/letter_to_{line_strip}.txt", "w") as new_letters:
+            new_letters.write(replaced_name)
+
+    invited_names.close()
