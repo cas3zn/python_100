c0af4152d51ec95f0e5d6c8a96cb0ea1cdb641ec
cookieflakes2 <101045565+cookieflakes2@users.noreply.github.com>
2022-07-10 15:00:24 +0300

    Update snake game

--- day21/snake.py
+++ day21/snake.py
@@ -35,6 +35,13 @@ class Snake:
         if self.head.heading() != DOWN:
             self.head.setheading(UP)
 
+    def reset(self):
+        for segment in self.segments:
+            segment.goto(1000, 1000)
+        self.segments.clear()
+        self.create_snake()
+        self.head = self.segments[0]
+
     def down(self):
         if self.head.heading() != UP:
             self.head.setheading(DOWN)
