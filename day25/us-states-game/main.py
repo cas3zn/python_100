964995455e6e91b77dc355632552eed454f81a33
cookieflakes2 <101045565+cookieflakes2@users.noreply.github.com>
2022-07-12 12:15:32 +0300

    Update U.S Map Quiz Game

--- day25/us-states-game/main.py
+++ day25/us-states-game/main.py
@@ -7,6 +7,7 @@ screen = turtle.Screen()
 
 image = "blank_states_img.gif"
 screen.title("U.S. States Game")
+screen.setup(width=750, height=500)
 screen.addshape(image)
 turtle.shape(image)
 states = pandas.read_csv("50_states.csv")
@@ -21,16 +22,22 @@ for x in states["state"]:
 while not game_over:
     answer = screen.textinput(title=f"{len(correct_guesses)}/50 States Correct", prompt="What's another state's "
                                                                                         "name?").title()
+    if answer == "Exit" or answer == "exit":
+        missing_states = []
+        for state in states_list:
+            if state not in correct_guesses:
+                missing_states.append(state)
+        new_data = pandas.DataFrame(missing_states)
+        new_data.to_csv("missing_states.csv")
+        break
     if answer in states_list:
         correct_guesses.append(answer)
         a = states[states.state == answer]
         x_pos = int(a.x)
         y_pos = int(a.y)
         position.go_to(pos_x=x_pos, pos_y=y_pos, state_name=answer)
-        print(correct_guesses)
 
     if len(correct_guesses) == 50:
         game_over = True
 
-
-screen.exitonclick()
+# states to learn.csv
