b43679600ada6a2a230981d077dff17676574385
Allan <allancheboiw0@gmail.com>
2023-02-17 17:06:39 +0800

    Day 57 - URL Building with Flask

--- day57/server.py
+++ day57/server.py
@@ -23,8 +23,9 @@ def guess(name):
     return render_template("guess.html", person_name=name, gender=gender, age=age)
 
 
-@app.route('/blog')
-def blog():
+@app.route('/blog/<num>')
+def get_blog(num):
+    print(num)
     blog_url = "https://api.npoint.io/c790b4d5cab58020d391"
     response = requests.get(blog_url)
     all_posts = response.json()
