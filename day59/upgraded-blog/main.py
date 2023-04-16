3cf4d433b69f9681dbb7659d9630b7564da7174a
Allan <allancheboiw0@gmail.com>
2023-04-16 16:53:54 +0800

    Fetch and render blog posts from API

--- day59/upgraded-blog/main.py
+++ day59/upgraded-blog/main.py
@@ -1,13 +1,34 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template
+import requests
 
 app = Flask(__name__)
+API_ENDPOINT = "https://api.npoint.io/ba2dda467100b9824e65"
+blog = requests.get(API_ENDPOINT).json()
 
 
 @app.route('/')
 def get_all_posts():
-    return render_template("index.html")
+    return render_template("index.html", all_posts=blog)
 
 
+@app.route('/about.html')
+def about_post():
+    return render_template("about.html")
+
+
+@app.route('/contact.html')
+def contact_post():
+    return render_template("contact.html")
+
+
+@app.route('/post/<int:index>')
+def show_post(index):
+    requested_post = None
+    for blog_post in blog:
+        if blog_post["id"] == index:
+            requested_post = blog_post
+
+    return render_template("post.html", post=requested_post)
 
 
 if __name__ == "__main__":
