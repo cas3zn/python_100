669b25baac511150dc169897fc0fc7fd061db05a
Allan <allancheboiw0@gmail.com>
2023-11-16 20:44:13 +0800

    using flask_sqlalchemy

--- 63-Library_Project_SQLite/main.py
+++ 63-Library_Project_SQLite/main.py
@@ -1,9 +1,37 @@
 from flask import Flask, render_template, request, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
 
+# create the extension
+db = SQLAlchemy()
+
+# create the app
 app = Flask(__name__)
+
+# configure the SQLite database, relative to the app instance folder
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///books-collection.db"
+
+# initialize the app with the extension
+db.init_app(app)
 
 all_books = []
 
+# Define the models
+class Books(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(250), nullable=False, unique=False)
+    author = db.Column(db.String(250), nullable=False, unique=False)
+    rating = db.Column(db.Float, nullable=False, unique=False)
+
+
+# create the table
+with app.app_context():
+    db.create_all()
+
+# create the record
+with app.app_context():
+    new_book = Books(id=11, title="Harry Potter", author="J. K. Rowling", rating=9.3)
+    db.session.add(new_book)
+    db.session.commit()
 
 @app.route('/')
 def home():
