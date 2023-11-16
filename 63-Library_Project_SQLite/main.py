25fdf31687646bb11f0bbe2031fe2fbd74fec187
Allan <allancheboiw0@gmail.com>
2023-11-16 16:56:27 +0800

    fetch input from the form

--- 63-Library_Project_SQLite/main.py
+++ 63-Library_Project_SQLite/main.py
@@ -1,18 +1,5 @@
 from flask import Flask, render_template, request, redirect, url_for
 
-'''
-Red underlines? Install the required packages first: 
-Open the Terminal in PyCharm (bottom left). 
-
-On Windows type:
-python -m pip install -r requirements.txt
-
-On MacOS type:
-pip3 install -r requirements.txt
-
-This will install the packages from requirements.txt for this project.
-'''
-
 app = Flask(__name__)
 
 all_books = []
@@ -23,9 +10,20 @@ def home():
     return render_template("index.html")
 
 
-@app.route("/add")
+@app.route("/add", methods=["GET", "POST"])
 def add():
-    pass
+    # if the request method is POST, fetch the input from the form as a dictionary and append into the all_books list
+    if request.method == "POST":
+        new_book = {
+            "title": request.form["title"],
+            "author": request.form["author"],
+            "rating": request.form["rating"]
+        }
+
+        all_books.append(new_book)
+
+        return redirect(url_for('home')) # redirects back to the home page after the form is submitted
+    return render_template("add.html")
 
 
 if __name__ == "__main__":
