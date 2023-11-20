ef4e1104e9d0030bacca011af25a4832b99bee28
Allan <allancheboiw0@gmail.com>
2023-11-21 02:27:45 +0800

    display on the home page

--- 64-Top-10-Movies-Website/main.py
+++ 64-Top-10-Movies-Website/main.py
@@ -23,6 +23,7 @@ db = SQLAlchemy()
 
 # create the app
 app = Flask(__name__)
+Bootstrap5(app)
 
 # configuring extension to the Flask app
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///a-100-movies-to-watch.db"
@@ -46,24 +47,31 @@ with app.app_context():
     db.create_all()
 
 # create a new record
-new_movie = Movie(
-    title="Phone Booth",
-    year=2002,
-    description="Publicist Stuart Shepard finds himself trapped in a phone booth, pinned down by an extortionist's sniper rifle. Unable to leave or receive outside help, Stuart's negotiation with the caller leads to a jaw-dropping climax.",
+'''
+second_movie = Movie(
+    title="Avatar The Way of Water",
+    year=2022,
+    description="Set more than a decade after the events of the first film, learn the story of the Sully family (Jake, Neytiri, and their kids), the trouble that follows them, the lengths they go to keep each other safe, the battles they fight to stay alive, and the tragedies they endure.",
     rating=7.3,
-    ranking=10,
-    review="My favourite character was the caller.",
-    img_url="https://image.tmdb.org/t/p/w500/tjrX2oWRCM3Tvarz38zlZM7Uc10.jpg"
+    ranking=9,
+    review="I liked the water.",
+    img_url="https://image.tmdb.org/t/p/w500/t6HIqrRAclMCA60NsSmeqe9RmNV.jpg"
 )
+
 
 # add it to the database
 with app.app_context():
-    db.session.add(new_movie)
+    db.session.add(second_movie)
     db.session.commit()
+'''
+    
 
 @app.route("/")
 def home():
-    return render_template("index.html")
+    result = db.session.execute(db.select(Movie))
+    all_movies = result.scalars()
+
+    return render_template("index.html", movies=all_movies)
 
 
 if __name__ == '__main__':
