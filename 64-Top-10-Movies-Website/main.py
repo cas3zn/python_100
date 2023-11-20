0c22035e8df0ec9ee6042506b4704ea0ec457635
Allan <allancheboiw0@gmail.com>
2023-11-21 02:02:13 +0800

    create the database

--- 64-Top-10-Movies-Website/main.py
+++ 64-Top-10-Movies-Website/main.py
@@ -18,11 +18,48 @@ pip3 install -r requirements.txt
 
 This will install the packages from requirements.txt for this project.
 '''
+# create the extension
+db = SQLAlchemy()
 
+# create the app
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
-Bootstrap5(app)
 
+# configuring extension to the Flask app
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///a-100-movies-to-watch.db"
+
+# initialize the app with the extension
+db.init_app(app)
+
+# define the models
+class Movie(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(250), unique=True, nullable=False)
+    year = db.Column(db.Integer, nullable=False)
+    description = db.Column(db.String(250), nullable=False)
+    rating = db.Column(db.Float, nullable=False)
+    ranking = db.Column(db.Integer, nullable=False)
+    review = db.Column(db.String(250), nullable=False)
+    img_url = db.Column(db.String(250), nullable=False)
+
+# create the table
+with app.app_context():
+    db.create_all()
+
+# create a new record
+new_movie = Movie(
+    title="Phone Booth",
+    year=2002,
+    description="Publicist Stuart Shepard finds himself trapped in a phone booth, pinned down by an extortionist's sniper rifle. Unable to leave or receive outside help, Stuart's negotiation with the caller leads to a jaw-dropping climax.",
+    rating=7.3,
+    ranking=10,
+    review="My favourite character was the caller.",
+    img_url="https://image.tmdb.org/t/p/w500/tjrX2oWRCM3Tvarz38zlZM7Uc10.jpg"
+)
+
+# add it to the database
+with app.app_context():
+    db.session.add(new_movie)
+    db.session.commit()
 
 @app.route("/")
 def home():
